322e08b09d3f1f3a23ea79769708b562b004590e
Bolan Oluwa <bolan@ellipweb.com>
2020-07-28 16:35:39 +0100

    WIP - Health care sector image

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -1,6 +1,6 @@
 import  React from "react";
 // import React.Dom from 'react.dom';
-
+import { Link } from "react-router-dom";
 class HealthComponent extends React.Component {
     constructor (public props: any) {
         super(props);
@@ -12,10 +12,14 @@ class HealthComponent extends React.Component {
     render() {
         return (
             <div>
-                <div className="_container"><h5>Health Sectors</h5></div>
+                <div className="_container"><h5>Health Sector</h5></div>
                 <div className="health_sector">
                     <img src="/images/healthcare.jpeg" alt="Health Care" />
                 </div>
+                <br />
+                <p>
+                    We will like to hear from Care Homes and Domiciliary Care Agencies. <Link to="/contactus">Contact us</Link>
+                </p>
             </div>
             
         );
